16f4f923f124b865be0feb91a3307d6b634e8f01
rajin9601 <rajin9601@naver.com>
2015-06-23 10:17:42 +0900

    2015 2 update 1

--- model/naive_snutt_data.js
+++ model/naive_snutt_data.js
@@ -74,6 +74,7 @@ NaiveLectureModel.prototype = {
     this._load_data(2014, 'W');
     this._load_data(2015, '1');
     this._load_data(2015, 'S');
+    this._load_data(2015, '2');
   },
   save: function (lectures, year, semester, callback) {
     /* This function saves this lecture and return a string id identifying lectures.
