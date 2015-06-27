b48af0776c5a95b6f1d281cad0115bae33e8159c
zxzl <hyungshike@gmail.com>
2015-06-28 02:10:45 +0900

    typed fetching

--- data/snutt/fetch.rb
+++ data/snutt/fetch.rb
@@ -22,76 +22,33 @@ elsif !["1", "2", "S", "W"].include?(semester) then
   exit!
 end
 
-category_map = {} #"xxx.xxx;yyy => "학문의 기초""
+general_types_to_code =
+{
+# 학문의 기초
+"foundation_writing"=>40,   #사고와 표현
+"foundation_language"=>41,  #외국어
+"foundation_math"=>42,      #수량적 분석과 추론
+"foundation_science"=>43,   #과학적 사고와 실험
+"foundation_computer"=>44,  #컴퓨터와 정보 활용
+# 학문의 세계
+"knowledge_literature"=>45, #언어와 문학
+"knowledge_art"=>46,        #문화와 예술
+"knowledge_history"=>47,    #역사와 철학
+"knowledge_politics"=>48,   #정치와 경제
+"knowledge_human"=>49,      #인간과 사회
+"knowledge_nature"=>50,     #자연과 기술
+"knowledge_life"=>51,       #생명과 환경
+# 선택교양
+"general_physical"=>52,     #체육
+"general_art"=>53,          #예술실기
+"general_college"=>54,      #대학과 리더십
+"general_creativity"=>55,   #창의와 융합
+"general_korean"=>56        #한국의 이해
+}
 
-#htm file download (wget)
-`sh gethtm.sh #{"#{year} #{semester}"}`
-Dir["htm/#{year}_#{semester}*.htm"].each do |htmfile|
-  result = `ruby parsing_htm.rb #{"#{htmfile}"}`.split("\n")
-  #학문의 기초 - ALL
-  if htmfile.include?("basics_all") then
-    result.each{|x| category_map[x] = "basics" unless category_map[x]}
-    #학문의 기초 - 외국어와 외국문화
-  elsif htmfile.include?("basics_foreign") then
-    result.each{|x| category_map[x] = "basics_foreign"}
-    #학문의 기초 - 국어와 작문
-  elsif htmfile.include?("basics_korean") then
-    result.each{|x| category_map[x] = "basics_korean"}
-    #학문의 기초 - 기초과학
-  elsif htmfile.include?("basics_science") then
-    result.each{|x| category_map[x] = "basics_science"}
 
-    #핵심교양 - 문학과 예술
-  elsif htmfile.include?("core_art") then
-    result.each{|x| category_map[x] = "core_art"}
-    #핵심교양 - 생명과 환경
-  elsif htmfile.include?("core_biology") then
-    result.each{|x| category_map[x] = "core_biology"}
-    #핵심교양 - 역사와 철학
-  elsif htmfile.include?("core_history") then
-    result.each{|x| category_map[x] = "core_history"}
-    #핵심교양 - 자연의 이해
-  elsif htmfile.include?("core_nature") then
-    result.each{|x| category_map[x] = "core_nature"}
-    #핵심교양 - 사회와 이념
-  elsif htmfile.include?("core_society") then
-    result.each{|x| category_map[x] = "core_society"}
-    #핵심교양 - 자연과 기술
-  elsif htmfile.include?("core_technology") then
-    result.each{|x| category_map[x] = "core_technology"}
 
-    #일반교양 - 문학과 예술
-  elsif htmfile.include?("normal_art") then
-    result.each{|x| category_map[x] = "normal_art"}
-    #일반교양 - 체육 및 기타
-  elsif htmfile.include?("normal_exercise") then
-    result.each{|x| category_map[x] = "normal_exercise"}
-    #일반교양 - 외국어와 외국문화
-  elsif htmfile.include?("normal_foreign") then
-    result.each{|x| category_map[x] = "normal_foreign"}
-    #일반교양 - 역사와 철학
-  elsif htmfile.include?("normal_history") then
-    result.each{|x| category_map[x] = "normal_history"}
-    #일반교양 - 국어와 작문
-  elsif htmfile.include?("normal_korean") then
-    result.each{|x| category_map[x] = "normal_korean"}
-    #일반교양 - 자연의 이해
-  elsif htmfile.include?("normal_nature") then
-    result.each{|x| category_map[x] = "normal_nature"}
-    #일반교양 - 기초과학
-  elsif htmfile.include?("normal_science") then
-    result.each{|x| category_map[x] = "normal_science"}
-    #일반교양 - 사회와 이념
-  elsif htmfile.include?("normal_society") then
-    result.each{|x| category_map[x] = "normal_society"}
-    #일반교양 - 기초교육 특별프로그램
-  elsif htmfile.include?("normal_special") then
-    result.each{|x| category_map[x] = "normal_special"}
-  end
-end
-puts "category mapping complete"
-
-#download
+#download whole coursebook
 puts "Start fetching...#{year}/#{semester}"
 
 xls_filename="#{Dir.getwd()}/xls/#{year}_#{semester}.xls"
@@ -117,10 +74,18 @@ open(xls_filename,"w") do |file|
 end
 puts "download complete : #{year}_#{semester}.xls"
 
-#convert
-puts "start converting from xls to txt"
-excel = Roo::Excel.new(xls_filename);
-m = excel.to_matrix
+#download coursebook for each type of general courses
+general_types_to_code.keys.each do |type|
+  xls_filename_typed="#{Dir.getwd()}/xls/#{year}_#{semester}_#{type}.xls"
+  data = "srchCond=1&pageNo=1&workType=EX&sortKey=&sortOrder=&srchOpenSchyy=#{year}&currSchyy=#{year}&srchOpenShtm=#{shtm}&srchCptnCorsFg=&srchOpenShyr=&srchSbjtCd=&srchSbjtNm=&srchOpenUpSbjtFldCd=&srchOpenSbjtFldCd=#{general_types_to_code[type]}&srchOpenUpDeptCd=&srchOpenDeptCd=&srchOpenMjCd=&srchOpenSubmattFgCd=&srchOpenPntMin=&srchOpenPntMax=&srchCamp=&srchBdNo=&srchProfNm=&srchTlsnAplyCapaCntMin=&srchTlsnAplyCapaCntMax=&srchTlsnRcntMin=&srchTlsnRcntMax=&srchOpenSbjtTmNm=&srchOpenSbjtTm=&srchOpenSbjtTmVal=&srchLsnProgType=&srchMrksGvMthd="
+  
+  res, data = http.post(path, data)
+  open(xls_filename_typed,"w") do |file|
+    file.print(res.body)
+  end
+  puts "download complete : #{year}_#{semester}_#{type}.xls"
+end
+
 
 def convert_classtime(time)
   wday = time[0]
@@ -147,59 +112,78 @@ def convert_classtime(time)
   return "#{wday}(#{from_ctime}-#{duration})"
 end
 
+# Converts i-th row of m into string. 
+# Returns "" for 교양 courses, if type is not given.
+def row_to_string(m, i, type="")
+  classification = m[i,0]
+  if classification == "교양" && type==""
+    return ""
+  end
+  department = m[i,2]
+  academic_year = m[i,3]
+  if academic_year == "학사"
+    academic_year = m[i,4]
+  end
+  course_number = m[i,5]
+  lecture_number = m[i,6]
+  course_title = m[i,7]
+  if m[i,8].to_s.length > 1
+    course_title = course_title + "(#{m[i,8]})"
+  end
+  course_title = course_title.gsub(/;/, ':')
+  credit = m[i,9].to_i
+  class_time = m[i,12]
+  location = m[i,14]
+  instructor = m[i,15]
+  quota = m[i,16].split(" ")[0].to_i
+  enrollment = m[i,17].to_i
+  remark = m[i,18].gsub(/
+\n/, " ")
+  category = type;
+  snuev_lec_id = snuev_eval_score = nil
+
+  #classtime 표기 통일
+  #수(7,8,9) -> 수(7-3)
+  class_time = class_time.split("/").map{|x| convert_classtime(x)}.join("/")
+
+  return "#{classification};#{department};#{academic_year};#{course_number};#{lecture_number};#{course_title};#{credit};#{class_time};#{location};#{instructor};#{quota};#{enrollment};#{remark};#{category};#{snuev_lec_id};#{snuev_eval_score}"
+end
+
+
+#convert
+puts "start converting from xls to txt"
+excel = Roo::Excel.new(xls_filename);
+m = excel.to_matrix
+
 open("#{txt_filename}.tmp", "w") do |file|
   file.puts "#{year}/#{semester}"
   file.puts Time.now.localtime().strftime("%Y-%m-%d %H:%M:%S")
   file.puts "classification;department;academic_year;course_number;lecture_number;course_title;credit;class_time;location;instructor;quota;enrollment;remark;category;snuev_lec_id;snuev_eval_score"
   3.upto(m.row_size-1) do |i|
-    classification = m[i,0]
-    department = m[i,2]
-    academic_year = m[i,3]
-    if academic_year == "학사"
-      academic_year = m[i,4]
+    str = row_to_string(m, i)
+    if str==""
+      next
     end
-    course_number = m[i,5]
-    lecture_number = m[i,6]
-    course_title = m[i,7]
-    if m[i,8].to_s.length > 1
-      course_title = course_title + "(#{m[i,8]})"
-    end
-    course_title = course_title.gsub(/;/, ':')
-    credit = m[i,9].to_i
-    class_time = m[i,12]
-    location = m[i,14]
-    instructor = m[i,15]
-    quota = m[i,16].split(" ")[0].to_i
-    enrollment = m[i,17].to_i
-    remark = m[i,18].gsub(/
-\n/, " ")
-    category = category_map["#{course_number};#{lecture_number}"]
-
-=begin
-    while true
-      snuev_api = nil
-      begin
-        snuev_api = JSON.parse(Net::HTTP.get(URI.parse(URI.escape("http://snuev.com/lecture/find?code=#{course_number}&professor=#{instructor}"))))
-      rescue
-        puts "fail"
-        snuev_api = nil
-      end
-      if snuev_api then
-        break
-      end
-    end
-    snuev_lec_id = snuev_api["lec_id"]
-    snuev_eval_score = (snuev_api["eval_point"].to_f / snuev_api["eval_count"].to_f).round(2)
-    puts "(#{i-2}/#{m.row_size-3}) #{course_number} #{instructor} #{course_title} : #{snuev_lec_id} : #{snuev_eval_score}"
-=end
-    snuev_lec_id = snuev_eval_score = nil
-
-    #classtime 표기 통일
-    #수(7,8,9) -> 수(7-3)
-    class_time = class_time.split("/").map{|x| convert_classtime(x)}.join("/")
-
-    file.puts "#{classification};#{department};#{academic_year};#{course_number};#{lecture_number};#{course_title};#{credit};#{class_time};#{location};#{instructor};#{quota};#{enrollment};#{remark};#{category};#{snuev_lec_id};#{snuev_eval_score}"
+    file.puts str
   end
+
+  puts "start putting general courses"
+  general_types_to_code.keys.each do |type|
+    xls_filename_typed="#{Dir.getwd()}/xls/#{year}_#{semester}_#{type}.xls"
+    # during summer/winter session, some type of general courses are not open
+    begin
+      excel = Roo::Excel.new(xls_filename_typed);
+      m = excel.to_matrix
+    rescue RuntimeError 
+      next
+    end
+    puts "putting #{type}"
+    3.upto(m.row_size-1) do |i|
+      str = row_to_string(m, i, type)
+      file.puts str
+    end
+  end
+
 end
 
 File.rename("#{txt_filename}.tmp", txt_filename)
