3a80a88a553ddf0105a0947812c9e5aad994d6ba
wafflestudio-master <master@wafflestudio.com>
2015-10-26 13:31:05 +0900

    2015 W update

--- model/naive_snutt_data.js
+++ model/naive_snutt_data.js
@@ -48,6 +48,7 @@ NaiveLectureModel.prototype = {
     this._load_data(2015, '1');
     this._load_data(2015, 'S');
     this._load_data(2015, '2');
+    this._load_data(2015, 'W');
   },
   save: function (lectures, year, semester, callback) {
     /* This function saves this lecture and return a string id identifying lectures.
