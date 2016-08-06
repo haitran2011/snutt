779d8f2f64b1310b22e871b1484c6cbba073ad59
Jang Ryeol <ryeolj5911@gmail.com>
2016-08-06 18:49:17 +0900

    example config

---
+++ config/config.js
@@ -0,0 +1,14 @@
+var config = {
+    production: process.env.SNUTT_PRODUCTION,
+    secretKey: process.env.SNUTT_SECRET,
+    host: process.env.SNUTT_HOST,
+    port: process.env.SNUTT_PORT,
+    protocol: process.env.SNUTT_PROTOCOL,
+    email: process.env.SNUTT_EMAIL,
+    ssl_key: process.env.SNUTT_SSL_KEY,
+    ssl_cert: process.env.SNUTT_SSL_CERT
+};
+
+if (config.production) process.env.NODE_ENV = "production";
+
+module.exports = config;
