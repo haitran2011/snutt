6b0f0ba629a8df3b178930ba329509a3d17b8891
Jang Ryeol <ryeolj5911@gmail.com>
2017-01-09 11:22:51 +0900

    fix unused imports removed issue (2)

--- app.ts
+++ app.ts
@@ -1,5 +1,5 @@
 import express = require("express");
-import db = require('./db');
+const db = require('./db');
 import logger = require("morgan");
 import cookieParser = require("cookie-parser");
 import bodyParser = require("body-parser");
