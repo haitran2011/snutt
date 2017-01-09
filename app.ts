7820d345ea9beb2f5465b524c1e9e054feeba6fe
Jang Ryeol <ryeolj5911@gmail.com>
2017-01-09 10:01:47 +0900

    view engine setup

--- app.ts
+++ app.ts
@@ -13,10 +13,8 @@ import fs = require('fs');
 var app = express();
 
 // view engine setup
-/*
-app.set('views', path.join(__dirname, 'views'));
+//app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
-*/
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
